d32cb335273b2572dc35b77d4ecc9dce8bcb6759
sabalegayatri23 <sabalegayatri23@gmail.com>
2025-09-22 10:57:59 +0530

    Add files via upload

---
+++ InterfaceExa.java
@@ -0,0 +1,41 @@
+interface Father
+{
+ public void bringwater();
+}
+interface Mother
+{
+public void bringwater();
+}
+yb6rclass Son implements Father, Mother
+{
+ public void bringwater()
+{
+System.out.println("Brought water for father and mother");
+}
+}
+ class Grandson1 extends Son
+{
+public void name()
+{
+System.out.println("ABC");
+}
+}
+ class Grandson2 extends Son
+{
+public void name()
+{
+System.out.println("PQR");
+}
+}
+public class InterfaceExa
+{
+public static void main(String args[])
+{
+Son s=new Son();
+s.bringwater(); 
+Grandson1 g1=new Grandson1();
+g1.name();
+Grandson2 g2=new Grandson2();
+g2.name();
+}
+}
